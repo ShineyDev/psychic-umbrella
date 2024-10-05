16eb7042f00552626ac578ba4e5e8af2a511309c
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2024-10-05 17:02:46 +0100

    collect base_hostname from deploy

--- build.py
+++ build.py
@@ -6,20 +6,19 @@ if TYPE_CHECKING:
 
 import os
 import shutil
+import sys
 
 import jinja2 as jinja
 
 
 path_static = "_static"
-build_environment = "production" if os.getenv("GITHUB_PAGES") else "development"
 
 
-def render_error(environment: Environment, /, *, code: int, message: str) -> str:
+def render_error(environment: Environment, base_hostname: str | None, /, *, code: int, message: str) -> str:
     template = environment.get_template("error.html")
 
     return template.render(
-        base_hostname="psychic-umbrella.shiney.dev",
-        build_environment=build_environment,
+        base_hostname=base_hostname,
         error_code=code,
         error_message=message,
         home_url="/",
@@ -27,25 +26,29 @@ def render_error(environment: Environment, /, *, code: int, message: str) -> str
     )
 
 
-def render_index(environment: Environment, /) -> str:
+def render_index(environment: Environment, base_hostname: str | None, /) -> str:
     template = environment.get_template("index.html")
 
     return template.render(
-        base_hostname="psychic-umbrella.shiney.dev",
-        build_environment=build_environment,
+        base_hostname=base_hostname,
         path_static=path_static,
     )
 
 
 def main():
+    try:
+        base_hostname = sys.argv[1]
+    except IndexError:
+        base_hostname = None
+
     environment = jinja.Environment(
         autoescape=jinja.select_autoescape(),
         loader=jinja.FileSystemLoader("template"),
     )
 
     pages = {
-        "404": render_error(environment, code=404, message="Whoops! This page doesn't exist."),
-        "index": render_index(environment),
+        "404": render_error(environment, base_hostname, code=404, message="Whoops! This page doesn't exist."),
+        "index": render_index(environment, base_hostname),
     }
 
     try:
